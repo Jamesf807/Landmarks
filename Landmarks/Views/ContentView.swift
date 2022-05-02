77b0f9b01ec82ebea119be3344a52c703780f8db
Jamesf807 <jamesfisher807@gmail.com>
2022-05-02 15:16:13 -0800

    Initial commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by James Fisher on 5/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
